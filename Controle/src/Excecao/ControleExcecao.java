c0a7a2915927cd7b6f04aa96e99628374eb79a1a
cleiber-mata <cleiber.mata@gmail.com>
2026-05-17 15:38:52 -0300

    Criação Git

---
+++ Controle/src/Excecao/ControleExcecao.java
@@ -0,0 +1,19 @@
+package Excecao;
+
+public class ControleExcecao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			int numero = Integer.parseInt("10");
+			int numeros[] = new int[10];
+			numeros[10] = 100;
+			System.out.println("Numero = " + numero);
+		} catch (NumberFormatException e) {
+			System.out.println("Numero Inválido!!!");
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("Elemento inexistente no array.");
+		}
+	}
+
+}
